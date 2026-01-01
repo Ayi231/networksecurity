4a26c08806e56cdb8d8e1ccd1f5447cd3bf74613
Ali <alielbeyali123@gmail.com>
2026-01-01 17:15:33 -0500

    Data Ingestion component

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -11,6 +11,7 @@ class NetworkSecurityException(Exception):
     def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,str(self.error_message))
     
+'''
 if __name__ == '__main__':
     try:
         logger.logging.info("enter try block")
@@ -18,3 +19,4 @@ if __name__ == '__main__':
         print('dss')
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+'''
